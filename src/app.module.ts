823c4cb9cfb96bca1bbaedc439249dc8f4e48fc7
Nkaltaeva <n.kaltaeva@ya.ru>
2023-05-03 19:39:15 +0300

    Nest1

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PostsModule } from './news/posts.module';
+import { CalcModule } from './calc/calc.module';
+
+
+@Module({
+  imports: [NewsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
